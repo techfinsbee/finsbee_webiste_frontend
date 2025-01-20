ea1d4eb48ece64c2db4edff778144e965aa82d03
pawang2005 <kumar.pawan2005g@gmail.com>
2025-01-20 17:53:37 +0530

    Sign Up

--- src/components/JoinFundMama.jsx
+++ src/components/JoinFundMama.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
-import './JoinFundMama.component.css';
 
 const JoinFundMama = () => {
   const { ref, inView } = useInView({
-    threshold: 1, // Trigger when 30% of the section is visible
-    triggerOnce: true, // Allow animation to re-trigger on each scroll
+    threshold: 1,
+    triggerOnce: true,
   });
 
   return (
     <>
-      <div ref={ref} style={{marginTop: "100px"}}>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div ref={ref} className="join-container">
+        <div className="header-container">
           <h1 className="head">
             Join FundsMama - Your First Step to Smarter Savings and Easier Investments
           </h1>
@@ -19,17 +18,107 @@ const JoinFundMama = () => {
         <div className="signup-div">
           <button
             className={`
-              absolute px-6 py-3
-              bg-[#E6D5C3] rounded-lg
+              signup-button
               transform transition-all duration-[1000ms]
-              ${inView ? '-translate-y-20 opacity-100' : 'translate-y-0 opacity-0'}
+              ${inView ? 'button-visible' : 'button-hidden'}
             `}
-            style={{ width: '110px', right: '40%' }}
           >
             SignUp
           </button>
         </div>
       </div>
+      <style jsx>{`
+        .join-container {
+          margin-top: 100px;
+        }
+
+        .header-container {
+          display: flex;
+          justify-content: center;
+        }
+
+        .head {
+          text-align: center;
+          width: 50%;
+          font-size: 30px;
+        }
+
+        .signup-button {
+          position: absolute;
+          padding: 0.75rem 1.5rem;
+          background-color: #E6D5C3;
+          border-radius: 0.5rem;
+          bottom: -45%;
+          right: 40%;
+          height: 70px;
+          width: 200px;
+          font-size: 30px;
+        }
+
+        .button-visible {
+          transform: translateY(-20px);
+          opacity: 1;
+        }
+
+        .button-hidden {
+          transform: translateY(0);
+          opacity: 0;
+        }
+
+        @media screen and (max-width: 728px) {
+          .head {
+            width: 100%;
+          }
+          
+          .signup-button {
+            right: 27%;
+            bottom: -150px;
+            transform: translateY(450px);
+          }
+          
+          .button-visible {
+            transform: translateY(430px);
+          }
+        }
+
+        @media screen and (max-width: 425px) {
+          .signup-button {
+            right: 25%;
+            bottom: -120px;
+            width: 180px !important;
+            height: 60px !important;
+            font-size: 26px;
+          }
+        }
+
+        @media screen and (max-width: 375px) {
+          .signup-button {
+            right: 23%;
+            bottom: 0px;
+            width: 160px !important;
+            height: 55px !important;
+            font-size: 24px;
+          }
+          
+          .button-visible {
+            transform: translateY(430px);
+          }
+        }
+
+        @media screen and (max-width: 320px) {
+          .signup-button {
+            right: 18%;
+            bottom: 0px;
+            width: 150px !important;
+            height: 50px !important;
+            font-size: 22px;
+          }
+          
+          .button-visible {
+            transform: translateY(430px);
+          }
+        }
+      `}</style>
     </>
   );
 };
