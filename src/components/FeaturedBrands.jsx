3e1ce9a012ca359a4c685cad1f54680017d6308c
pawang2005 <kumar.pawan2005g@gmail.com>
2025-01-28 15:58:36 +0530

    Tab view updated

--- src/components/FeaturedBrands.jsx
+++ src/components/FeaturedBrands.jsx
@@ -8,7 +8,7 @@ const FeaturedBrands = ({allLogos}) => {
       <h2 className="text-[#006d5b] text-6xl font-bold mb-6 center" style={{textAlign:"center"}}>
           {allLogos.title}
         </h2>
-      <div className="max-w-8xl feature-brand" style={{display:"flex", flexDirection:"row", alignItems:"center", gap:"40px"}}>
+      <div className="max-[90vw] feature-brand" style={{display:"flex", flexDirection:"row", alignItems:"center", gap:"40px"}}>
         <div className="flex flex-wrap gap-4">
           <BrandSlider allLogos={allLogos.logos}></BrandSlider>
         </div>
