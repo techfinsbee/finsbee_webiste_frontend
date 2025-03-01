c3dfe71deb0a4aa6459fd5c524cf072d658ab32b
pawang2005 <kumar.pawan2005g@gmail.com>
2025-03-01 11:20:57 +0530

    Update made

--- src/components/LendingPartner.jsx
+++ src/components/LendingPartner.jsx
@@ -2,12 +2,32 @@
 import HomeHeader from "./Home/HomeHeader";
 const LendingPartner = () => {
   const LendingPartners = [
-    {partner:"Olyv", link: "http://olyv.co.in",img:'/lending_partner2.png'},
-    {partner:"Meghdoot Mercantile", link:"https://meghdootmercantile.com",img:'/lending_partner6.png'},
-    {partner:"Chintamani", link:"https://chintamanifinlease.com",img:'/lending_partner7.png'},
-    {partner:"Girdhar Finlease", link:"#",img:'/lending_partner1.png'},
-    {partner:"Payme", link:"https://paymeindia.in",img:'/lending_partner4.png'},
-    {partner:"Capri Global", link:"https://capriloans.in",img:'/lending_partner.png'},
+    {
+      partner: "Olyv",
+      link: "http://olyv.co.in",
+      img: "/lending_partner2.png",
+    },
+    {
+      partner: "Meghdoot Mercantile",
+      link: "https://meghdootmercantile.com",
+      img: "/lending_partner6.png",
+    },
+    {
+      partner: "Chintamani",
+      link: "https://chintamanifinlease.com",
+      img: "/lending_partner7.png",
+    },
+    { partner: "Girdhar Finlease", link: "#", img: "/lending_partner1.png" },
+    {
+      partner: "Payme",
+      link: "https://paymeindia.in",
+      img: "/lending_partner4.png",
+    },
+    {
+      partner: "Capri Global",
+      link: "https://capriloans.in",
+      img: "/lending_partner.png",
+    },
   ];
   const dropdownData = [
     {
@@ -16,11 +36,11 @@ const LendingPartner = () => {
     },
     {
       title: "Loans",
-      link: "loan-section",
+      link: "loan-section-home",
     },
     {
       title: "Mart",
-      link: "mart",
+      link: "mart-home",
     },
     {
       title: "About Us",
@@ -28,46 +48,44 @@ const LendingPartner = () => {
     },
     {
       title: "Contact Us",
-      link: "contact-us",
+      link: "contact-us-home",
     },
   ];
   return (
     <>
-         <HomeHeader
-          dropdownData={dropdownData}
-          COLOR="#fff"
-          Hover="home"
-          TXTCOLOR="#"
-        ></HomeHeader>
+      <HomeHeader
+        dropdownData={dropdownData}
+        COLOR="#fff"
+        Hover="home"
+        TXTCOLOR="#"
+      ></HomeHeader>
 
-    <section className="py-16 px-4">
-      <div className="max-w-6xl mx-auto">
-        <h1 className="text-4xl md:text-5xl font-bold text-center mb-12 text-gray-800">
-          Our Lending Partners
-        </h1>
-        
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
-          {LendingPartners.map((info, index) => (
-            <a 
-              key={index}
-              href={info.link} 
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block"
-            >
-              
-              <div className="bg-[#09615D] p-4 rounded-lg text-white hover:shadow-lg transition-shadow duration-300 border border-gray-100 h-full flex items-center justify-center group gap-2">
-              <img src={info.img} alt=""  className="w-[8vw]"/>
-                <div className="text-xl font-medium  group-hover:text-gray-200 transition-colors duration-300 text-center">
-                  
-                  {info.partner}
+      <section className="py-16 px-4">
+        <div className="max-w-6xl mx-auto">
+          <h1 className="text-4xl md:text-5xl font-bold text-center mb-12 text-gray-800">
+            Our Lending Partners
+          </h1>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+            {LendingPartners.map((info, index) => (
+              <a
+                key={index}
+                href={info.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block"
+              >
+                <div className="bg-[#09615D] p-4 rounded-lg text-white hover:shadow-lg transition-shadow duration-300 border border-gray-100 h-full flex items-center justify-center group gap-2 flex-col md:flex-row">
+                  <img src={info.img} alt="" className="w-[20vw] md:w-[8vw]" />
+                  <div className="text-xl font-medium  group-hover:text-gray-200 transition-colors duration-300 text-center">
+                    {info.partner}
+                  </div>
                 </div>
-              </div>
-            </a>
-          ))}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
