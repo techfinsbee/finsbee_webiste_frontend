ced74bd1d284c22ad8a618eed563d6f4498b2cda
fundsmamaTech <tech@fundsmama.com>
2025-07-28 13:24:02 +0530

    Update LendingPartners.jsx

--- src/components/LendingPartners.jsx
+++ src/components/LendingPartners.jsx
@@ -35,7 +35,7 @@ const LendingPartners = () => {
   { name: "Shriram Finance", logo: "https://tse3.mm.bing.net/th?id=OIP.ppWNoxao1V5UpbEjS0R1AQHaCd&pid=Api&P=0&h=180" },
   { name: "Poonawalla", logo: "https://tse3.mm.bing.net/th?id=OIP.NzIweLQ9xIgSeVfITHc4XQHaEH&pid=Api&P=0&h=180" },
   { name: "IIFL", logo: "https://tse1.mm.bing.net/th?id=OIP.MSHYCH8sKpEsk2M3cilIeQHaDW&pid=Api&P=0&h=180" },
-   { name: "IIFL", logo: "https://freeimage.host/i/FvX4Wfj" },
+   { name: "IIFL", logo: "https://iili.io/FvX4Wfj.png" },
 ];
 
   // Animation settings
