ee1e7dae60c2c9469b7d031ceed07a7b913ee3fc
Nikhil7458 <rudhraFM@gmail.com>
2025-10-14 18:01:53 +0530

    build iisue solve

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -1,5 +1,4 @@
-import BookingHero from '@/Booking_Components/BookingHero'
-import PlansSection from '@/Booking_Components/PlansSection'
+
 import React from 'react'
 
 export default function page() {
